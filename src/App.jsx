5855eb37acf610201088c81b7c548903cc5deff8
James Drake <info@jamesmdrakemedia.com>
2024-06-13 16:24:54 -0700

    feat: initial route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom"
+
 import "./App.css"
 
 function App() {
-  return <></>
+  return <><Outlet /></>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
+
 import './index.css'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />,
+  children: []
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider route={router} />
   </React.StrictMode>,
 )
