755bb93f1f1be4e8df10da489786d1bcaa0c8f43
James Drake <info@jamesmdrakemedia.com>
2024-06-13 17:50:37 -0700

    feat: footer color adjustments and prettier formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
 export function Footer() {
-    return (
-        <>
-            <section className="w-full h-72 bg-accent-700 text-primary-50">
-            <div className="w-3/4 mx-auto h-full flex flex-row">
-            <div className="w-1/3 p-10"><h3>Navigation</h3></div>
-            <div className="w-1/3 p-10"><h3>Connect with me</h3></div>
-            <div className="w-1/3 p-10"><h3>Posts</h3></div>
-            </div>
-            </section>
-            <section className="w-full h-10 bg-primary-800 text-primary-200 text-sm text-center flex flex-col justify-center"><p>&copy;2024 James M. Drake, Jr. All rights reserved.</p></section>
-        </>
-    )
+  return (
+    <>
+      <section className="h-72 w-full bg-accent-700 text-primary-50">
+        <div className="mx-auto flex h-full w-3/4 flex-row">
+          <div className="w-1/3 p-10">
+            <h3>Navigation</h3>
+          </div>
+          <div className="w-1/3 p-10">
+            <h3>Connect with me</h3>
+          </div>
+          <div className="w-1/3 p-10">
+            <h3>Posts</h3>
+          </div>
+        </div>
+      </section>
+      <section className="flex h-10 w-full flex-col justify-center bg-primary-950 text-center text-sm text-primary-200">
+        <p>&copy;2024 James M. Drake, Jr. All rights reserved.</p>
+      </section>
+    </>
+  )
 }
