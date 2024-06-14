b725328103a509e81bb3953ad341b1168c7f4398
James Drake <info@jamesmdrakemedia.com>
2024-06-14 14:55:45 -0700

    fix: img link fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export function Hero() {
   return (
     <section className="aspect-video w-screen bg-primary-950">
-      <img className="" src="public/Screen Shot 2024-06-13 at 7.26.10 PM.png" alt="" />
+      <img className="" src="/Screen Shot 2024-06-13 at 7.26.10 PM.png" alt="" />
     </section>
   )
 }
