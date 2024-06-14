ed5dc425502fe2a290ae1a28b1ecbd32965ebdc1
James Drake <info@jamesmdrakemedia.com>
2024-06-14 12:44:14 -0700

    feat: hero styles and placeholder image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,7 @@
 export function Hero() {
-  return <section className="h-screen w-screen bg-primary-950">test</section>
+  return (
+    <section className="aspect-video w-screen bg-primary-950">
+      <img className="" src="public/Screen Shot 2024-06-13 at 7.26.10 PM.png" alt="" />
+    </section>
+  )
 }
