6974ded97d4f9946bb2508378eda734a6703e953
James Drake <info@jamesmdrakemedia.com>
2024-06-14 14:53:21 -0700

    feat: footer links and social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,43 @@
+import { IconContext } from "react-icons"
+import { FaXTwitter, FaInstagram, FaFacebookF } from "react-icons/fa6"
+
 export function Footer() {
   /* eslint-disable-next-line no-undef */
   const version = APP_VERSION
 
   return (
     <>
-      <section className="h-72 w-full bg-accent-700 text-primary-50">
+      <section className="h-60 w-full bg-accent-700 text-primary-50">
         <div className="mx-auto flex h-full w-3/4 flex-row">
           <div className="w-1/3 p-10">
-            <h3>Navigation</h3>
+            <h3 className="mb-2 text-xl font-bold">Links</h3>
             <ul>
-              <li>Top</li>
-              <li>About</li>
+              <li>Home</li>
+              <li>About Me</li>
               <li>Credits</li>
               <li>Contact</li>
             </ul>
           </div>
           <div className="w-1/3 p-10">
-            <h3>Connect with me</h3>
+            <h3 className="mb-2 text-xl font-bold">Connect with me</h3>
+
+            <IconContext.Provider value={{ size: "2rem" }}>
+              <div className="flex w-full flex-row gap-3">
+                <a href="https://www.instagram.com/jamesdrakedp">
+                  <FaInstagram />
+                </a>
+                <a href="">
+                  <FaFacebookF />
+                </a>
+                <a href="">
+                  <FaXTwitter />
+                </a>
+              </div>
+            </IconContext.Provider>
           </div>
           <div className="w-1/3 p-10">
-            <h3>Posts</h3>
+            <h3 className="mb-2 text-xl font-bold">Posts</h3>
+            <p>Coming soon!</p>
           </div>
         </div>
       </section>
