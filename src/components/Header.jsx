2227099912dbb63be2389d9fa10497b054461c20
James Drake <info@jamesmdrakemedia.com>
2024-06-14 12:40:24 -0700

    fix: header style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 export function Header() {
   return (
-    <section className="fixed h-14 w-full border-solid bg-primary-950 text-primary-50">
+    <section className="fixed flex h-14 w-full flex-col justify-center bg-primary-950/50 text-primary-50">
       <div className="mx-auto flex w-5/6 flex-row justify-between">
-        <div>James M. Drake, Jr.</div>
         <div>
+          <div>
+            <div className="mb-0 text-2xl font-bold leading-none">James M. Drake, Jr.</div>
+            <div className="mt-0 text-xs leading-none">Cinematographer & Camera Operator</div>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center">
           <ul className="l flex list-none flex-row justify-evenly gap-6 align-middle">
             <li>About</li>
             <li>Credits</li>
