14966f46ef83f75ac8a7dca577234665cab98464
James Drake <info@jamesmdrakemedia.com>
2024-06-13 17:51:00 -0700

    feat: initial contact config

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+export function Contact() {
+  return (
+    <section className="h-72 w-full text-primary-950">
+      <div className="mx-auto flex h-full w-1/2 flex-col justify-evenly text-center align-middle">
+        <h2>Reach Out!</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, quod suscipit voluptates amet ipsa quasi
+          quae vitae tempora cumque eius.
+        </p>
+        <button className="mx-auto h-10 w-32 bg-accent-600 text-primary-100 hover:bg-accent-500 active:bg-accent-400">
+          Contact Me
+        </button>
+      </div>
+    </section>
+  )
+}
