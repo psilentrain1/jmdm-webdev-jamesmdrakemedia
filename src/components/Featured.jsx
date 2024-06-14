2fd74e726895c8160d9e150c93eded29bf82def2
James Drake <info@jamesmdrakemedia.com>
2024-06-14 13:37:42 -0700

    feat: featured section style

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,8 +1,22 @@
 export function Featured() {
   return (
     <section className="h-80 w-full bg-accent-700 text-primary-50">
-      <div className="mx-auto flex h-full w-3/4 flex-row text-center">
-        <h2 className="mx-auto">Featured Projects</h2>
+      <div className="mx-auto h-full w-3/4 py-10 text-center">
+        <h2 className="mx-auto mb-4 text-2xl font-bold">Featured Projects</h2>
+        <div id="feat-container" className="flex w-full flex-row gap-8">
+          <article className="flex aspect-video w-72 flex-col justify-center bg-primary-950 text-primary-50 text-opacity-0 hover:text-opacity-100">
+            <h3>90 Day Fiance</h3>
+            <p>Camera Operator</p>
+          </article>
+          <article className="flex aspect-video w-72 flex-col justify-center bg-primary-950 text-primary-50 text-opacity-0 hover:text-opacity-100">
+            <h3>90 Day Fiance</h3>
+            <p>Camera Operator</p>
+          </article>
+          <article className="flex aspect-video w-72 flex-col justify-center bg-primary-950 text-primary-50 text-opacity-0 hover:text-opacity-100">
+            <h3>90 Day Fiance</h3>
+            <p>Camera Operator</p>
+          </article>
+        </div>
       </div>
     </section>
   )
