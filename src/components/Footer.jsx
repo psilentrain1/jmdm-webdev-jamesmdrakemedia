15b67ea2d341387933b1f69cd96807ed05f15224
James Drake <info@jamesmdrakemedia.com>
2024-06-13 17:27:58 -0700

    feat: initial footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export function Footer() {
+    return (
+        <>
+            <section className="w-full h-72 bg-accent-700 text-primary-50">
+            <div className="w-3/4 mx-auto h-full flex flex-row">
+            <div className="w-1/3 p-10"><h3>Navigation</h3></div>
+            <div className="w-1/3 p-10"><h3>Connect with me</h3></div>
+            <div className="w-1/3 p-10"><h3>Posts</h3></div>
+            </div>
+            </section>
+            <section className="w-full h-10 bg-primary-800 text-primary-200 text-sm text-center flex flex-col justify-center"><p>&copy;2024 James M. Drake, Jr. All rights reserved.</p></section>
+        </>
+    )
+}
