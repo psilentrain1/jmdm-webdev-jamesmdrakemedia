c2b644ec2ae8b2ab300d21ddc045a52294cbfaeb
James Drake <info@jamesmdrakemedia.com>
2024-06-15 15:29:42 -0700

    feat: about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
 export function About() {
   return (
-    <section id="About" className="h-80 w-full bg-primary-50 text-primary-950">
+    <section id="About" className="h-96 w-full bg-primary-50 text-primary-950">
       <div className="mx-auto flex h-full w-3/4 flex-row text-center">
         <div className="w-1/2 p-10">
-          <div className="mx-auto aspect-video w-96 bg-primary-950">Placeholder</div>
+          <div className="mx-auto aspect-[7/5] w-96 bg-[url(About-720x540.jpeg)] bg-cover"></div>
         </div>
         <div className="w-1/2 p-10 text-left">
           <h2 className="mb-4 text-2xl font-bold">A little about me</h2>
