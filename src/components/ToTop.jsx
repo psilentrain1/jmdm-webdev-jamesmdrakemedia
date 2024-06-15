6ecb918632cf4480f2eae63b1d64d6db22ea2a7f
James Drake <info@jamesmdrakemedia.com>
2024-06-15 14:49:51 -0700

    feat: work on future ToTop

--- src/components/ToTop.jsx
+++ src/components/ToTop.jsx
@@ -1,12 +1,38 @@
+import { HashLink } from "react-router-hash-link"
 import { IconContext } from "react-icons"
 import { IoIosArrowDropup } from "react-icons/io"
 
+/* let scrollPos = 0
+const Top = document.getElementById("Top")
+
+function checkPosition() {
+  const windowY = window.scrollY
+  if (windowY < scrollPos) {
+    // scrolling up
+    Top.classList.add("opacity-100")
+    Top.classList.remove("opacity-0")
+  } else {
+    // scrolling down
+    Top.classList.add("opacity-0")
+    Top.classList.add("opacity-100")
+  }
+  scrollPos = windowY
+}
+
+window.addEventListener("scroll", checkPosition)
+
+// reference: https://dev.to/changoman/showhide-element-on-scroll-w-vanilla-js-3odm */
+
 export function ToTop() {
   return (
-    <div className="fixed bottom-6 right-6 flex h-16 w-12 flex-col justify-center bg-accent-700 text-primary-50 shadow-lg hover:bg-accent-500">
-      <IconContext.Provider value={{ size: "2rem", className: "mx-auto" }}>
-        <IoIosArrowDropup />
-      </IconContext.Provider>
-    </div>
+    <HashLink to="#top">
+      <div
+        id="ToTop"
+        className="fixed bottom-6 right-6 flex h-16 w-12 flex-col justify-center bg-accent-700 text-primary-50 shadow-lg hover:bg-accent-500">
+        <IconContext.Provider value={{ size: "2rem", className: "mx-auto" }}>
+          <IoIosArrowDropup />
+        </IconContext.Provider>
+      </div>
+    </HashLink>
   )
 }
