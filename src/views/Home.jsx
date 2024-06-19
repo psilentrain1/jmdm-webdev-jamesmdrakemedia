1c6a7df2ee4e93c365b87f875b933dc9e5890759
James Drake <info@jamesmdrakemedia.com>
2024-06-19 12:46:45 -0700

    disable right click on home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { Header } from "../components/Header.jsx"
 import { Hero } from "../components/Hero.jsx"
 import { About } from "../components/About.jsx"
@@ -6,6 +7,16 @@ import { Contact } from "../components/Contact.jsx"
 import { Footer } from "../components/Footer.jsx"
 
 export function Home() {
+  useEffect(() => {
+    const handleContextMenu = e => {
+      e.preventDefault()
+    }
+    document.addEventListener("contextmenu", handleContextMenu)
+    return function cleanup() {
+      document.removeEventListener("contextmenu", handleContextMenu)
+    }
+  }, [])
+
   return (
     <>
       <Header />
