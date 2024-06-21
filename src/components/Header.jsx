e24f9fc44862f79d9cdacb09818577c6e4c70d93
James Drake <info@jamesmdrakemedia.com>
2024-06-21 12:05:21 -0700

    redraw page when screen size changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { HashLink as Link } from "react-router-hash-link"
 import { slide as Menu } from "react-burger-menu"
 import { FiMenu } from "react-icons/fi"
+import { useLayoutEffect, useState } from "react"
 
 function NavMenuDesktop() {
   return (
@@ -29,9 +30,21 @@ function NavMenuMobile() {
 }
 
 export function Header() {
+  const [size, setSize] = useState([])
   const screenSizeSmall = 640
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight])
+    }
+    window.addEventListener("resize", updateSize)
+    updateSize()
+    return () => window.removeEventListener("resize", updateSize)
+  }, [])
+
   return (
-    <section className="fixed top-0 flex h-14 w-full flex-col justify-center bg-primary-950 text-primary-50">
+    <section
+      className="fixed top-0 flex h-14 w-full flex-col justify-center bg-primary-950 text-primary-50"
+      width={size[0]}>
       <div className="mx-auto flex w-5/6 flex-row justify-between">
         <div className="w-3/4 md:w-1/2">
           <Link to="/#Hero">
