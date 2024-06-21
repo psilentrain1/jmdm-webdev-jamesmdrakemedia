e49358b337f411d11fcdd43d85718ba8da560fd9
James Drake <info@jamesmdrakemedia.com>
2024-06-20 22:31:00 -0700

    hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,35 @@
 import { HashLink as Link } from "react-router-hash-link"
+import { slide as Menu } from "react-burger-menu"
+
+function NavMenuDesktop() {
+  return (
+    <ul className="l flex list-none flex-row justify-evenly gap-6 align-middle">
+      <li className="transition-all hover:underline">
+        <Link to="/#About">About</Link>
+      </li>
+      <li className="transition-all hover:underline">
+        <Link to="/#Contact">Contact</Link>
+      </li>
+      <li className="transition-all hover:underline">
+        <Link to="/Credits">Credits</Link>
+      </li>
+    </ul>
+  )
+}
+
+function NavMenuMobile() {
+  return (
+    <Menu right>
+      <Link to="/#About">About</Link>
+      <Link to="/#Contact">Contact</Link>
+      <Link to="/Credits">Credits</Link>
+    </Menu>
+  )
+}
 
 export function Header() {
   // TODO: Add logic to hide the normal nav menu for smaller screens and show the hamburger menu
+  // 640px breakpoint - screen.width <= 640
 
   return (
     <section className="fixed top-0 flex h-14 w-full flex-col justify-center bg-primary-950 text-primary-50">
@@ -15,17 +43,7 @@ export function Header() {
           </Link>
         </div>
         <div className="flex flex-col justify-center">
-          <ul className="l flex list-none flex-row justify-evenly gap-6 align-middle">
-            <li className="transition-all hover:underline">
-              <Link to="/#About">About</Link>
-            </li>
-            <li className="transition-all hover:underline">
-              <Link to="/#Contact">Contact</Link>
-            </li>
-            <li className="transition-all hover:underline">
-              <Link to="/Credits">Credits</Link>
-            </li>
-          </ul>
+          {screen.width <= 640 ? <NavMenuMobile /> : <NavMenuDesktop />}
         </div>
       </div>
     </section>
